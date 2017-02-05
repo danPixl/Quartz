e2d05417a5b08e351ff4414f64a8a8ce1ecd61e9
danPixl <danny@vinci.cloud>
2017-02-04 23:27:03 -0700

    do things

--- renderer.js
+++ renderer.js
@@ -1,3 +1,12 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+var fs = require('fs');
+if (fs.existsSync("quartz-server.txt")) {
+    fs.readFile("quartz-server.txt", 'utf8', function(err, data) {
+      console.log('OK: ' + "quartz-server.txt");
+      console.log(data)
+      const server_url = data
+    });
+}
